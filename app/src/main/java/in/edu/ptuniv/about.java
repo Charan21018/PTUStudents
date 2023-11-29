b36f769cfff9355dfcd403e36ef8256a7d8e6c01
kishen2003 <kishen2003@gmail.com>
2023-11-29 23:53:11 +0530

    activity

--- app/src/main/java/in/edu/ptuniv/about.java
+++ app/src/main/java/in/edu/ptuniv/about.java
@@ -1,8 +1,11 @@
 package in.edu.ptuniv;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class about extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class about extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        Button frag=findViewById(R.id.buttonfrag);
+        frag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                aboutFragment aboutFragment=new aboutFragment();
+                FragmentManager manager=getSupportFragmentManager();
+                manager.beginTransaction().replace(R.id.frame,aboutFragment.class,null).commit();
+
+            }
+        });
+
     }
 }
